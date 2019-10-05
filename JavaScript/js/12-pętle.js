a337cd5bc7b430a676d9c6b264e88ee367118862
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-05 16:58:57 +0200

    pętle

--- JavaScript/js/12-pętle.js
+++ JavaScript/js/12-pętle.js
@@ -6,5 +6,33 @@ const imiona = [
 ];
 
 for(let i = 0; i <= imiona.length-1; i++) {
+    //if(i%2 == 0){
+       // continue;
+        //break
+   // }
     console.log(imiona[i]);
 }
+
+
+// for in - jedyna pętla do iteracji po obiektach
+const obj = {
+imie: "Adam",
+nazwisko: "Nowacki",
+wiek: 36
+}
+
+for(let i in obj) {
+    console.log(`Właściwość: ${i} ::::::: Wartość właściwości: ${obj[i]}`);
+}
+
+// pętla while
+let number = 10;
+let iter = 0;
+let wynik = [];
+
+while(iter <= number) {
+    wynik.push(`iteracja nr ${iter + 1}`);
+    iter++;
+}
+//console.log(wynik.join(" :::: "));
+console.log(wynik.join(" \n "));

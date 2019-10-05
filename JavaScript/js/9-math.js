71fb0715a31ad3861dc5e3eb1618f435192cf5fb
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-05 15:42:22 +0200

    math javascript

---
+++ JavaScript/js/9-math.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.random());
+
+// zaokrąglanie w dół
+console.log(Math.floor(9.8));
+
+// zaokrąglanie w górę
+console.log(Math.ceil(Math.PI));
+
+// zaokrąglenie matematyczne
+console.log(Math.round(1.52));
+
+// potęgowanie dwie wersje
+console.log(Math.pow(2,3));
+console.log(2**3);

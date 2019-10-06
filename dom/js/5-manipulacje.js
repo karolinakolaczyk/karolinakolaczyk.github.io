b298c0a125de1d2ed2f2531e5f9c2d156bdf3010
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-06 14:56:42 +0200

    eventy js

--- dom/js/5-manipulacje.js
+++ dom/js/5-manipulacje.js
@@ -10,13 +10,19 @@ console.log(mainHeaderInner);
 
 window.addEventListener("scroll", () => {
     if(document.documentElement.scrollTop >= (2 * window.innerHeight) ) {
+        console.log(mainHeader.dataset.category);
+        mainHeader.style.fontSize = "40px";
         document.body.classList.add("scroll-2");
         document.body.classList.remove("scroll");
     
     } else if(document.documentElement.scrollTop >= window.innerHeight) {
+        console.log(mainHeader.dataset.category);
+        mainHeader.style.fontSize = "13px";
         document.body.classList.add("scroll");
         document.body.classList.remove("scroll-2");
     } else {
+        console.log(mainHeader.dataset.category);
+        mainHeader.style.fontSize = "initial";
         document.body.classList.remove("scroll", "scroll-2");
     }
 })

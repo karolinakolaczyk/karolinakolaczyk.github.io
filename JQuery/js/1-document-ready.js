51a85cba50d812d005e99f762a40ab121fd90140
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-12 10:51:40 +0200

    2-selektory

--- JQuery/js/1-document-ready.js
+++ JQuery/js/1-document-ready.js
@@ -6,9 +6,11 @@ $(function() {
     //alert("test");
     $("#first").css({color: "red"}).hide("slow").show(2500);
 
-    //let second = $("#second");
-    //let kolekcjaParagraf = $(".paragraf");
-    //console.log(kolekcjaParagraf);
+    let second = $("#second");
+    let kolekcjaParagraf = $(".paragraf");
+    console.log(kolekcjaParagraf);
     // $(".paragraf") = jQuery(".paragraf")  $ = jQuery
+
+
 }
 );
---
+++ JQuery/js/2-selektory.js
@@ -0,0 +1,12 @@
+
+//pobieranie elementów po znaczniku
+var paragraphs = $("p");
+console.log(paragraphs);
+
+let allElements = $("*");
+console.log(allElements);
+
+let srcAtrybut = $("[src]");
+console.log(srcAtrybut);
+
+//pobieranie elementów po id

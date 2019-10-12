d70026b85480cc1c9406eb45b35d188edc3ba55a
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-12 11:28:50 +0200

    dalej

---
+++ JQuery/js/3-manipulacja-dom.js
@@ -0,0 +1,35 @@
+
+//Pobieranie tekstu
+let tekstParagrafuPierwszego = $("#paragraf").text();
+console.log(tekstParagrafuPierwszego);
+
+//Dodawanie tekstu
+$(".paragraf").eq(1).text("Ala ma kota");
+
+
+//Dodawanie HTML
+$(".paragrafnext").html("To jest <strong>paragraf</strong> z dodanym <br> HTML");
+
+//Dodawanie treści na końcu selektora
+$(".paragrafnext").append("Treść na końcu selektora");
+
+//Dodawanie treści za selektorem
+$(".paragrafnext").after("Treść za selektorem");
+
+$(".paragrafnext").prepend("Treść na początku selektora");
+
+$(".paragrafnext").before("Treść przed selektorem");
+
+
+//Usuwanie elementu i jego zawartości
+$("strong").remove();
+
+//Usuwanie TYLKO jego zawartości
+$(".paragraf").empty();
+
+//Dodawanie CSS
+$(".paragrafnext").css({ 'font-size': '3em' , 'color': 'red' });
+
+//Dodawanie klasys
+$("h1").addClass("blue");
+

4f6398d317ecab00406c8b264d1d349eb49b5bf1
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-12 14:07:30 +0200

    animacje

---
+++ JQuery/js/6-animacje.js
@@ -0,0 +1,15 @@
+$(function() {
+    'use strict';
+
+
+    //Efekt hide i show
+$("p").hide(3000).show(3000);
+
+
+//Efekt fadeOut i fadeIn
+$("h1").fadeOut(3000).fadeIn(3000);
+
+//Efekt slideUp i slideDown
+$("h2").slideUp(3000).slideDown(3000);
+
+});

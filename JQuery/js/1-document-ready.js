5a457b6e59e7f06e89373e9b66218989b1b520d0
Karolina Kołaczyk <k.kolaczyk@hotmail.com>
2019-10-12 10:43:34 +0200

    Update 1-document-ready.js

--- JQuery/js/1-document-ready.js
+++ JQuery/js/1-document-ready.js
@@ -3,7 +3,12 @@
 //Funkcja document.ready() - wersja skrócona
 $(function() {
 
-    alert("test");
-    // $("#first").css{color: "red"});
+    //alert("test");
+    $("#first").css({color: "red"}).hide("slow").show(2500);
+
+    //let second = $("#second");
+    //let kolekcjaParagraf = $(".paragraf");
+    //console.log(kolekcjaParagraf);
+    // $(".paragraf") = jQuery(".paragraf")  $ = jQuery
 }
 );
